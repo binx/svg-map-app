d0526523e14493ab2ae61ef17b8e3ba4d217c74c
Rachel Binx <rbinx@netflix.com>
2019-04-16 18:55:14 -0700

    minor formatting on params

--- src/components/Params.js
+++ src/components/Params.js
@@ -1,31 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { Form, Select, Button, InputNumber } from "antd";
+import { Select, Button, InputNumber } from "antd";
+const Option = Select.Option;
 
 class Params extends Component {
   state = {};
- 
+
   render() {
-    const features = this.props.data
-    const keys = []
-        features.forEach(function(f){
-            const fprops = Object.keys(f.properties);
-            fprops.forEach(fp => keys.push(fp))  
-        })
-    const dedupe = ([...new Set(keys)])
-    return(
+    const features = this.props.data;
+    const keys = [];
+    features.forEach(function(f) {
+      const fprops = Object.keys(f.properties);
+      fprops.forEach(fp => keys.push(fp));
+    });
+    const dedupe = [...new Set(keys)];
+
+    return (
       <div>
-      <Select name="thisID" style= {{width: 120}} >
-      { dedupe.map(k => <option value = {k}>{k}</option>)}
-      </Select>
-      <span>
-      <InputNumber />
-      <InputNumber />
-      </span>
-      <Button type="primary" htmlType="submit">Set Parameters</Button>
+        <Select name="thisID" style={{ width: 120 }}>
+          {dedupe.map((k, i) => (
+            <Option value={k} key={`option${i}`}>
+              {k}
+            </Option>
+          ))}
+        </Select>
+        <span>
+          <InputNumber />
+          <InputNumber />
+        </span>
+        <Button type="primary" htmlType="submit">
+          Set Parameters
+        </Button>
       </div>
-      )
-    }
+    );
+  }
 }
 
 export default Params;
