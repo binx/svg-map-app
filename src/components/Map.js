1803776703bfea009a8dcb7ab933ab7a788cef60
Ingrid Burrington <lifewinning@gmail.com>
2019-05-21 15:05:52 -0400

    getting tiles with sphericalmercator instead of d3-tile

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import centroid from "@turf/centroid";
 import { tile } from "d3-tile";
 import * as d3 from "d3";
+import * as SphericalMercator from "@mapbox/sphericalmercator";
 
 class Map extends Component {
   state = {};
@@ -22,23 +23,11 @@ class Map extends Component {
 
     const svg = d3.select(this.refs.svg)
 
-    const mercator = d3.geoMercator()
-      .translate([width / 2, height / 2])
-      .center(mapCentroid.geometry.coordinates)
-      .clipExtent([[0,0],[width,height]])
-      .fitExtent(
-        [
-          [width * .05, height * .05],
-          [width - (width * .05), height - (height * .05)]
-        ],
-        mapData
-      );
-
     const projection = d3.geoOrthographic()
       .center(mapCentroid.geometry.coordinates)
       .translate([width/4,height/4]) //I honestly don't know why this works
       .rotate([-mapCentroid.geometry.coordinates[0], -mapCentroid.geometry.coordinates[1]])
-      //.clipExtent([[0,0],[width,height]])
+      .clipExtent([[0,0],[width,height]])
       .fitExtent(
           [
             [width * .05, height * .05],
@@ -46,87 +35,136 @@ class Map extends Component {
           ],
           mapData
         )
+    const initialScale = projection.scale()
 
-    const projscale = projection.scale();
-    const path = d3.geoPath().projection(projection);
+    const path = d3.geoPath(projection)
 
+    const getTiles = () => {
+    console.log(projection.scale())
+    const tiles = [];
+    const z = (0 | Math.log(projection.scale()) / Math.LN2) - 6
+
+    const upperbound = projection.invert([0,0])
+    const lowerbound = projection.invert([width,height])
     
+    console.log(projection(upperbound), projection(lowerbound))
+    console.log(z)
+    console.log(upperbound,lowerbound)
 
+    const merc = new SphericalMercator({
+      size: 256
+    })
+
+    const xyz = merc.xyz([upperbound[0], upperbound[1], lowerbound[0], lowerbound[1]], z)
+
+    const rows = d3.range(xyz.minX, xyz.maxX +1)
+    const cols = d3.range(xyz.minY, xyz.maxY+1)
+
+    cols.forEach(function(c) {
+      rows.forEach(function(r){
+        tiles.push({
+          x: r,
+          y: c, 
+          z: z
+        })
+      })
+    })
     
-    const drawTiles = () => {
-
-      const merctile = tile()
-      .size([width, height])
-      .scale(mercator.scale() * (2 * Math.PI))
-      .translate(mercator([0, 0]))
-
-      console.log(mercator.scale())
-      console.log(merctile())
-
-    const mapTiles = merctile().map(async d => {
+    let tileID = tiles.map(t => `tile-${t.x}-${t.y}-${t.z}`) 
+      this.setState({tiles: tileID})
+      console.log(tileID)
+    
+    const mapTiles = Promise.all(tiles.map(async d => {
       d.data = await d3.json(`https://tile.nextzen.org/tilezen/vector/v1/256/all/${d.z}/${d.x}/${d.y}.json?api_key=ztkh_UPOQRyakWKMjH_Bzg`);
       return d;
+    }))
+  
+  const tilewrap = svg.selectAll('#tileWrap')
+
+  tilewrap.selectAll('g').remove()
+
+  tilewrap.selectAll('g').data(tileID).enter().append('g').attr('id',function(t){return t})
+
+   mapTiles.then(function(ti){
+    ti.forEach(function(tile){
+      let arr = zenArray(tile)
+      svg.select(`#tile-${tile.x}-${tile.y}-${tile.z}`)
+      .selectAll('path')
+      .data(arr)
+      .enter().append("path")
+          .attr("d", path)
+          .attr("class", function(d) {return getClass(d)})
+          .exit();
+      })
     })
-
-    Promise.all(mapTiles).then(ti => {
-      const mapTiles = ti.map(t => {
-          const obj = {};
-          obj.coords = `tile-${t.x}-${t.y}-${t.z}`
-          obj.mapTile = this.zenArray(t).map(d => ({
-            d: path(d),
-            class: this.getClass(d),
-          }))
-          return obj;
-        });
-        this.setState({
-          tiles: mapTiles,
-          outline: path(mapData)
-        });
-    })
-
-    svg.selectAll('path').attr('transform','')
-
-    }
-
-    drawTiles();
-
-    const zoomies = () => {
-      mercator.scale(mercator.scale() * d3.event.transform.k)
-      .translate([d3.event.transform.x, d3.event.transform.y])
-
-      projection
-        .scale(projection.scale() * d3.event.transform.k)
-        .translate([d3.event.transform.x, d3.event.transform.y])
-
-      svg.selectAll('path').attr("transform",d3.event.transform)
-      this.setState({outline: path(mapData)})
-      console.log(d3.event.transform)
-
-      //svg.selectAll('.tile').attr('transform', "")
-    }
-
-    const zoom = d3.zoom()
-      .on("zoom", zoomies)
-      .on("end", drawTiles)
-    svg.call(zoom);
-   
+   this.setState({outline: path(mapData)})
+   d3.selectAll('g').attr('transform',"")
   }
 
-  getClass = d => {
+  getTiles()
+
+ let rotate0, coords0;
+ const coords = () => projection.rotate(rotate0).invert([d3.event.x, d3.event.y]);
+
+  // svg.call(zoom);
+
+  svg
+    .call(d3.drag()
+      .on('start', () => {
+        rotate0 = projection.rotate();
+        coords0 = coords();
+      })
+      .on('drag', () => {
+        const coords1 = coords();
+        projection.rotate([
+          rotate0[0] + coords1[0] - coords0[0],
+          rotate0[1] + coords1[1] - coords0[1],
+        ])
+        this.setState({outline: path(mapData)})
+        getTiles()
+      })
+      .on('end', () => {
+        getTiles()
+      })
+      )
+   const zoomies = () => {
+    projection
+      //.scale(initialScale *d3.event.transform.k)
+      .fitExtent(
+          [
+            [(width*d3.event.transform.k * .05), (height *d3.event.transform.k * .05)],
+            [width*d3.event.transform.k - (width*d3.event.transform.k * .05), height*d3.event.transform.k - (height*d3.event.transform.k * .05)]
+          ],
+          mapData
+        )
+    
+    svg.selectAll('g').attr('transform', d3.event.transform)
+    console.log(d3.event.transform)
+    console.log(projection.scale())
+    
+    this.setState({outline: path(mapData)})
+  }
+    const zoom = d3.zoom()
+      //.scaleExtent(projection.scale(), projection.scale*1.5)
+      .on("zoom", zoomies)
+      .on("end", getTiles)
+  svg.call(zoom)
+
+ const getClass = d => {
     let kind = d.properties.kind || '';
     if (d.properties.boundary)
       kind += '_boundary';
     return `${d.layer_name}-layer ${kind}`;
   }
 
-  zenArray = t => {
+const zenArray = t => {
     let features = [];
     const layers = ['water', 'landuse', 'roads', 'buildings'];
     layers.forEach(function(layer) {
       if (t.data[layer]) {
         for (let i in t.data[layer].features) {
           // Don't include any label placement points
-          //if(t.data[layer].features[i].properties.label_placement) { continue }
+          if(t.data[layer].features[i].properties.label_placement) { continue }
 
           // // Don't show large buildings at z13 or below.
           // if(zoom <= 13 && layer == 'buildings') { continue }
@@ -144,7 +182,7 @@ class Map extends Component {
       0
     ));
   }
-
+}
   render() {
     const { tiles = [], outline } = this.state;
     const { width, height } = this.props.mapSettings;
@@ -154,13 +192,7 @@ class Map extends Component {
         ref="svg"
         style={{ margin: "20px" }}
       >
-       {tiles.map((g,i) => (
-          <g key={g.coords} className="tile" id={g.coords}>
-         { g.mapTile.map((path,j) => (
-              <path key={`path${i}${j}`} className={path.class} d={path.d} id={path.coords}/>
-          ))}
-          </g>
-          ))}
+      <g id = "tileWrap" className="tile"></g>
         <path className="site" d={outline} id="site" ref="outline"/>
       </svg>
     );
